39167823b77c82cf7b1d27f9cac3455eec28d3e5
ntzwilly <ntzwilly@gmail.com>
2023-01-28 19:20:10 +0200

    add create users controller and create action to register user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      token = encode_token({ user_id: @user.id })
+      render json: { @user, token: token}, status: :ok
+    else 
+      render json: { error: "Invalid username or password"}, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params 
+    params.require(:user).permit([:username, :password])
+  end
+end
