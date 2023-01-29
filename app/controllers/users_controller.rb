122069d5d5a01380f94c71faf74d83a3ff9a9231
ntzwilly <ntzwilly@gmail.com>
2023-01-29 12:45:36 +0200

    add index method to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+    render json: @users, status: 200
+  end
+
   def create
     @user = User.create(user_params)
     if @user.valid?
